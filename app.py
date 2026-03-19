748784731e8ad4855fe924a012b77ed9fd217954
Tanvi Rao <raotanvi29@gmail.com>
2026-03-19 13:57:16 +0530

    fixed origins typo

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import requests
 import os
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origin": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.after_request
 def after_request(response):
