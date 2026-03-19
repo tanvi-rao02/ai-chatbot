35c12d026d2d7bac503409989e559e4861870ff0
Tanvi Rao <raotanvi29@gmail.com>
2026-03-19 13:47:47 +0530

    added cors fix

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ import requests
 import os
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origin": "*"}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origin": "*"}})
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+    return response
 
 API_KEY = os.getenv("API_KEY")
 
